db08162c03bf1df685b375fd4fbddecb19fb677f
Leon <leon8082@126.com>
2020-07-16 16:38:17 +0800

    Created an interface and an abstract class for class what should be initialized

---
+++ CommonLib/Interfaces/IInitializable.cs
@@ -0,0 +1,23 @@
+﻿namespace CommonLib.Interfaces
+{
+    /// <summary>
+    /// 表示类型可以被初始化（准备资源）和终结（释放资源）
+    /// </summary>
+    public interface IInitializable
+    {
+        /// <summary>
+        /// 获取是否已经初始化成功的标志
+        /// </summary>
+        bool IsInitialized { get; }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// 终结并清理资源
+        /// </summary>
+        void Terminate();
+    }
+}
